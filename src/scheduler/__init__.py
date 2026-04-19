38ce897b70e9d9ac3862b6b15f5aa2b385c25ba4
Omega <emomega@proton.me>
2026-04-19 17:44:32 -0400

    Create __init__.py

---
+++ src/scheduler/__init__.py
@@ -0,0 +1,2 @@
+# This file initializes the scheduler module, allowing the rest of the code to be imported as part of the package.
+# It could also contain configuration settings or global variables related to scheduling.
